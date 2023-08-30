5139a7d60e4847f799df0b10667f5d9d9afbd0a8
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2023-08-30 14:06:32 -0500

    Initial EmbedMe testing

---
+++ src/test/java/org/eclipse/jetty/demos/EmbedMeTest.java
@@ -0,0 +1,66 @@
+//
+//  ========================================================================
+//  Copyright (c) Mort Bay Consulting Pty Ltd and others.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.demos;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.component.LifeCycle;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+public class EmbedMeTest
+{
+    private Server server;
+
+    @BeforeEach
+    public void startServer() throws Exception
+    {
+        server = EmbedMe.newServer(0);
+        server.start();
+    }
+
+    @AfterEach
+    public void stopServer()
+    {
+        LifeCycle.stop(server);
+    }
+
+    @Test
+    public void testGetWelcome() throws IOException, InterruptedException
+    {
+        HttpClient client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(server.getURI().resolve("/"))
+            .GET()
+            .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+        assertThat(response.statusCode(), is(200));
+        assertThat(response.body(), containsString("<title>Content from WEB-INF/html</title>"));
+    }
+}
